d752cf7a6418e0a7721ded47cc38dbb680ab0a9e
MONOUE Simon Ulrich <simonulrich56@gmail.com>
2023-08-30 09:42:32 -0400

    maj

--- js/dataList.js
+++ js/dataList.js
@@ -2,19 +2,30 @@ projects = [
     {
         "title": "Chantier",
         "slug": "Application de gestion de chantier",
+        "url":"http://chorusarchi.com/",
         "img_intro":"./img/projects/chantier-app.png",
         "techno_web": 'Coding',
         "desc": "Une application de gestion de chantier qui permet de suivre les différentes tâches, les ressources utilisées, les délais, etc...",
         "img_details":"./img/projects/chantier-app.png",
         "details": [
             {
-                "name": 'Page présentation',
-                "desc": 'Cette page présente sommairement les catégories de produit proposées sur la plateforme.',
+                "name": 'Gestion des projets et des équipes',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
                 "img": 'images/modern-shop-1.png'
             },
             {
-                "name": 'Page produit',
-                "desc": 'Cette page présente les produits par catégorie.',
+                "name": 'Suivi des tâches et des délais',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/modern-shop-2.png'
+            },
+            {
+                "name": 'Gestion des ressources et des fournitures',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/modern-shop-2.png'
+            },
+            {
+                "name": 'Rapports et statistiques',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
                 "img": 'images/modern-shop-2.png'
             }
         ],
@@ -22,6 +33,7 @@ projects = [
     },
     {
         "title": "JBOS",
+        "url":"https://my-jbos.net/",
         "slug": "Application de gestion d'établissement primaire",
         "img_intro":"./img/projects/lumino-app.png",
         "techno_web": 'Coding',
@@ -29,88 +41,120 @@ projects = [
         "img_details":"./img/projects/lumino-app.png",
         "details": [
             {
-                "name": 'Page présentation',
-                "desc": 'Cette page d\'accueil a été conçue ainsi dans le but de refletter le professionnalisme et la rigueur dont fait face cette entreprise quand il s\'agit de travailler. Elle Présente également de façon sommaire les différents services que propose cette entreprise.',
+                "name": 'Gestion des élèves et des enseignants',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
+            },
+            {
+                "name": 'Gestion des emplois du temps',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
+            },
+            {
+                "name": 'Système de notation',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
+            },
+            {
+                "name": 'Communication avec les parents',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
                 "img": 'images/sofinez.png'
             }
         ],
         
     },
     {
-        "title": "Medical Game",
-        "slug": "medical-game",
-        "img_intro":"images/medicalGame-intro.jpg",
-        "techno_web": 'React JS',
-        "desc_min": "Medical Game est une plateforme d'apprentissage des concept de base dans le domaine de la santé.",
-        "desc": "Cette plateforme a été conçue pour permettre aux utilisateurs (enfants, adolescents, parents) d'apprendre rapidement et facilement des notions dans le domaine de la santé d'où cette facilité de manipulation des composants, cette rapidité à accéder à l'information.",
-        "img_details":"images/medicalGame-1.png",
+        "title": "WEMOS",
+        "url":"https://wemos.net/",
+        "slug": "Application de gestion de point de vente, de stock, de commande & vente en ligne d'une quincaillerie",
+        "img_intro":"./img/projects/wemos-app-1.png",
+        "techno_web": 'Coding',
+        "desc": "Une application de gestion de point de vente qui permet de gérer les stocks, les ventes, les paiements, etc....",
+        "img_details":"./img/projects/wemos-app-1.png",
         "details": [
             {
-                "name": 'Page de création compte',
-                "desc": 'Cette page utilisée pour la création de compte permet l\'acquisition des informations de l\'utilisateur, nécessaire la proposition du contenu à l\'utilisateur en fonction des informations récupérées.',
-                "img": 'images/medicalGame.png'
+                "name": 'Gestion des produits et des stocks',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
             },
             {
-                "name": 'Page de connexion',
-                "desc": 'Cette page permet à l\'utilisateur d\'accéder à son espace de travail en renseignant ses informations de connexion valide.',
-                "img": 'images/medicalGame-2.png'
+                "name": 'Enregistrement des ventes et des paiements',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
             },
             {
-                "name": "Page modules d'apprentissage",
-                "desc": 'Il s\'agit de la page affichant en fonction de certains paramètres de l\'utilisateur, les différents modules d\'apprentissage qu\'il devra parcourir.',
-                "img": 'images/medicalGame-3.png'
+                "name": 'Gestion des promotions et des remises',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
             },
             {
-                "name": "Page details module d'apprentissage",
-                "desc": 'Il s\'agit de la page présentant le détails du module d\'apprentissage sous forme de vidéo et regroupé en étape, pour donner à l\'utilisateur une impression de progression dans ses compétences. Pour faciliter l\'enseignement une approche par ',
-                "img": 'images/medicalGame-4.png'
+                "name": 'Rapports de vente',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
             }
         ],
         
     },
     {
-        "title": "Power H",
-        "slug": "power-h",
-        "img_intro":"images/powerH-intro.jpg",
-        "techno_web": 'React JS',
-        "desc_min": "Power H est une plateforme permettant la gestion d'établissement hotélière.",
-        "desc": "",
-        "img_details":"images/powerH-intro.jpg",
+        "title": "ZBidAlley",
+        "url":"https://zbidalley.com/",
+        "slug": "Application de vente aux enchères",
+        "img_intro":"./img/projects/zbidalley-app.png",
+        "techno_web": 'Coding',
+        "desc": "Une application de vente aux enchères en ligne qui permet aux utilisateurs de vendre et d'acheter des produits par le biais d'enchères.",
+        "img_details":"./img/projects/zbidalley-app.png",
         "details": [
             {
-                "name": 'Page de connexion',
-                "desc": '',
-                "img": 'images/powerH.png'
+                "name": 'Création et gestion d\'enchères',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
             },
             {
-                "name": 'Page d\'accueil',
-                "desc": '',
-                "img": 'images/powerH-1.png'
+                "name": 'Système d\'enchères en temps réel',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
             },
             {
-                "name": "Page module gestion logements & résidences",
-                "desc": '',
-                "img": 'images/powerH-2.png'
+                "name": 'Profils d\'utilisateurs et évaluations',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
             },
             {
-                "name": "Page module gestion résidences",
-                "desc": '',
-                "img": 'images/powerH-3.png'
+                "name": 'Paiements sécurisés',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
+            }
+        ],
+        
+    },
+    {
+        "title": "WILMA",
+        "url":"https://wilmasarl.com",
+        "slug": "Site vitrine pour entreprise de vente de matériel médical",
+        "img_intro":"./img/projects/wilma-website.png",
+        "techno_web": 'WordPress',
+        "desc": "Un site vitrine pour une entreprise spécialisée dans la vente de matériel médical, présentant les produits et les services proposés.",
+        "img_details":"./img/projects/wilma-website.png",
+        "details": [
+            {
+                "name": 'Page d\'accueil avec une présentation de l\'entreprise et de son équipe',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
             },
             {
-                "name": "Page module gestion caisses & facturations",
-                "desc": '',
-                "img": 'images/powerH-4.png'
+                "name": 'Pages de services détaillées',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
             },
             {
-                "name": "Page module gestion caisses",
-                "desc": '',
-                "img": 'images/powerH-5.png'
+                "name": 'Témoignages clients',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
             },
             {
-                "name": "Page module facturation",
-                "desc": '',
-                "img": 'images/powerH-6.png'
+                "name": 'Page de contact pour les demandes de consultation',
+                "desc": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.',
+                "img": 'images/sofinez.png'
             }
         ],
         
